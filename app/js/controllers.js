e5b365b3751b37920219818ae04fd61af772dbdc
Bruce Washburn <btwashburn@gmail.com>
2016-05-25 15:08:44 -0700

    update files on the server

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,8 +8,7 @@ undercountingControllers.controller('OrganizationListController', ['$scope','Org
 	$scope.orderProp = 'name';
 }]);
 
-undercountingControllers.controller('OrganizationDetailController', ['$scope', '$routeParams', 'Organization',
-                                                                     function($scope, $routeParams, Organization) {
+undercountingControllers.controller('OrganizationDetailController', ['$scope', '$routeParams', 'Organization',function($scope, $routeParams, Organization) {
 	$scope.organization = Organization.get({orgId: $routeParams.orgId});
 	$scope.test
 	$scope.visible = false;
@@ -21,15 +20,26 @@ undercountingControllers.controller('OrganizationDetailController', ['$scope', '
 undercountingControllers.controller('profileEditor', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
 
 	$http.get('organizations/' + $routeParams.orgId + '.json').success(function(data) {
+	  console.log('refreshing scope.organization with organizations/' + $routeParams.orgId + '.json');
 		$scope.organization = data;
 	});
+	
+	$scope.routerHost = "sanmateo-32.dev.oclc.org";
+	$scope.routerPort = ":3072";
 
-	 $scope.saveOrganization = function() {
-		 console.log('we made it!') 
-		 //$http.post("scripts/router.js", {'urls':profile.url})
-	  };
-	}]);
+	$scope.saveOrganization = function() {
+		console.log($scope.organization);
+		var router = "http://"+$scope.routerHost+$scope.routerPort;
+		$http.post(router, $scope.organization)
+			.then(function success(response) {
+				console.log(response.data);
+			}, function error(response) {
+				console.log(response.status);
+			}); 
+	};
+	
+}]);
 
 undercountingControllers.run(function(editableOptions) {
 	editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
-});  
+});
